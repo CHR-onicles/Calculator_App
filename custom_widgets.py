9c7aa15eafb631140303aeef2320bfc7da2efb7a
CHR-onicles <tpandivine48@gmail.com>
2021-05-28 00:31:20 +0000

    Update custom_widgets.py

--- custom_widgets.py
+++ custom_widgets.py
@@ -78,3 +78,32 @@ class NoCursorLineEdit(QLineEdit):
 
 
 
+class BlurEffect(QGraphicsBlurEffect):
+    effectRect = None
+
+    def setEffectRect(self, rect):
+        self.effectRect = rect
+        self.update()
+
+    def draw(self, qp):
+        if self.effectRect is None or self.effectRect.isNull():
+            # no valid effect rect to be used, use the default implementation
+            super().draw(qp)
+            print('bao')
+        else:
+            qp.save()
+            # clip the drawing so that it's restricted to the effectRect
+            qp.setClipRect(self.effectRect)
+            # call the default implementation, which will draw the effect
+            super().draw(qp)
+            # get the full region that should be painted
+            fullRegion = QRegion(qp.viewport())
+            # and subtract the effect rectangle
+            fullRegion -= QRegion(self.effectRect)
+            qp.setClipRegion(fullRegion)
+            # draw the *source*, which has no effect applied
+            self.drawSource(qp)
+            qp.restore()
+
+
+
