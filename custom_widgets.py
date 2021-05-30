e34475aa013ef2b6101375901f970cbaf826afe4
CHR-onicles <tpandivine48@gmail.com>
2021-05-30 12:37:20 +0000

    added number validator for calc screen

--- custom_widgets.py
+++ custom_widgets.py
@@ -120,8 +120,54 @@ class HSeparationLine(QFrame):
         return
 
 
+class NumberInputValidator(QValidator):
+    """
+    Custom validator class to validate number input in Calc screen.
+    """
 
-# THIS BREAKS MY APPS UI
+    def validate(self, v_string: str, index: int):
+        # invalid_chars =
+        # `~!@#$%^&*()-_=+{[}]|\\'",<>/?
+        state = None
+        if len(v_string) < 4 or v_string[:4] != 'GH₵ ':
+            # ic('Less than 4 or not GHC - invalid')
+            state = QValidator.Invalid
+
+        if len(v_string) >= 4:
+            # ic('Greater than 4 - VALID')
+            state = QValidator.Intermediate
+
+        if (v_string[4:].isdigit()) is True:
+            # ic('All digits - VALID')
+            state = QValidator.Acceptable
+
+        for x in v_string[4:]:
+            if ((x.isdigit() is False) and '.' not in v_string[4:]) or x.isalpha():
+                # ic('Alpha or Anything else - invalid')
+                state = QValidator.Invalid
+
+        nums = v_string[4:].split('.')
+        if '.' in v_string[4:]:
+            if (nums[0].isdigit() and nums[1].isdigit()) is True:
+                # ic('Has period and both numbers are legit - VALID')
+                state = QValidator.Acceptable
+            if '.' in v_string[4:] and len(v_string.split('.')[1]) >= 1:
+                if (nums[0].isdigit() is True) and (nums[1].isdigit() is False):
+                    # ic('Has period, but other chars too - invalid')
+                    state = QValidator.Invalid
+            if len(nums[1]) > 2:
+                # ic('More than 2 digits after period - invalid')
+                state = QValidator.Invalid
+
+        if v_string.count('.') > 1:
+            # ic('More than one period - invalid')
+            state = QValidator.Invalid
+
+        return state, v_string, index
+
+
+
+# THIS BREAKS MY APPS UI - It's supposed to realign button icons with text.
 # class ProxyStyle(QProxyStyle):
 #     def drawControl(self, element, option, painter, widget=None):
 #         if element == QStyle.CE_PushButtonLabel:
